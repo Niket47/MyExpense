e971c791528baca57a7bb15316a84fcb3489398e
Niket dadhaniya <123920570+jeerencoder@users.noreply.github.com>
2023-09-30 06:51:03 +0530

    search applyed

--- Expense-App/Screens/Home.js
+++ Expense-App/Screens/Home.js
@@ -8,6 +8,7 @@ import {
   View,
   Image,
   TouchableOpacity,
+  TextInput,
 } from 'react-native';
 import React, { useState } from 'react';
 import TransactionCard from '../Compooents/TransactionCard';
@@ -16,13 +17,7 @@ import { deleteExpense } from '../Redux/Slices';
 import HomeHader from '../Compooents/HomeHader';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import TranCard from '../Compooents/TranCard';
-import {
-  ActivityIndicator,
-  Button,
-  Chip,
-  List,
-  TextInput,
-} from 'react-native-paper';
+import { ActivityIndicator, Button, Chip, List } from 'react-native-paper';
 import CatFilterBox from '../Compooents/CatFilterBox';
 
 const Home = ({ navigation }) => {
@@ -33,13 +28,13 @@ const Home = ({ navigation }) => {
 
   const allcat = ['All', 'Income', 'Expense', 'Transaction'];
 
-  const filteredData =
-    selectedCategory === 'All'
-      ? data
-      : data.filter(item => item.category == selectedCategory);
+  // const filteredData =
+  //   selectedCategory === 'All'
+  //     ? data
+  //     : data.filter(item => item.category == selectedCategory);
 
   console.log(data, 'data');
-  console.log(filteredData, 'flllll');
+  // console.log(filteredData, 'flllll');
 
   const onDelete = itemData => {
     console.log(itemData.item.id, 'object-pressd');
@@ -49,24 +44,40 @@ const Home = ({ navigation }) => {
     navigation.navigate('Income', { expenseId: itemData.item.id });
   };
 
-  // console.log(condt(items));
-
-  const [text, setText] = useState('');
-
-  const filteredbyName = data.filter(item =>
-    item.name.toLowerCase().includes(text.toLowerCase()),
-  );
-  console.log(filteredbyName, 'filteredbyName');
-
-  const filteredbyDate = data.filter(item => {
-    const itemDate = new Date(item.date).toLocaleDateString(); // Format the date for comparison
-    return itemDate.includes(text);
-  });
+  const [searchtext, setSearchtext] = useState('');
 
   const opendrawer = () => {
     navigation.openDrawer();
   };
   const notification = () => {};
+
+  // const filtersss = data.filter(item => {
+  //   const name = item.name.toLowerCase().includes(searchtext.toLowerCase());
+  //   const itemDate = new Date(item.date).toLocaleDateString();
+
+  //   const dec = item.description
+  //     .toLowerCase()
+  //     .includes(searchtext.toLowerCase());
+  //   return name || dec;
+  // });
+
+  // console.log(filtersss, 'filtersss');
+
+  const filterData = (searchtext, category) => {
+    return data.filter(item => {
+      const name = item.name.toLowerCase().includes(searchtext.toLowerCase());
+      const dec = item.description
+        .toLowerCase()
+        .includes(searchtext.toLowerCase());
+      const categoryMatch =
+        category === 'All' || item.category == selectedCategory;
+
+      return (name || dec) && categoryMatch;
+    });
+  };
+
+  const filteredDatass = filterData(searchtext, selectedCategory);
+  console.log(filteredDatass, 'filterData');
 
   const renderExpenseItem = itemData => {
     return (
@@ -74,7 +85,6 @@ const Home = ({ navigation }) => {
         amountcolor={
           itemData.item.category === 'Income' ? '#00A86B' : '#DE2402'
         }
-        // iconname={itemData.item.category == 'Income' ? 'up' : 'down'}
         iconname={
           itemData.item.category == 'Income'
             ? 'up'
@@ -104,8 +114,8 @@ const Home = ({ navigation }) => {
           showsVerticalScrollIndicator={false}>
           <View>
             <HomeHader
-              onleftpress={notification}
-              onrightpress={opendrawer}
+              onleftpress={opendrawer}
+              onrightpress={notification}
               lefticonname="menufold"
               iconname="search1"
               title="nik"
@@ -113,9 +123,10 @@ const Home = ({ navigation }) => {
           </View>
           <View>
             <TextInput
+              style={styles.input}
               label="Search by any..."
-              value={text}
-              onChangeText={text => setText(text)}
+              value={searchtext}
+              onChangeText={text => setSearchtext(text)}
             />
           </View>
           <View style={styles.btnview}>
@@ -133,7 +144,7 @@ const Home = ({ navigation }) => {
 
           <View>
             <FlatList
-              data={filteredData}
+              data={filteredDatass}
               scrollEnabled={false}
               renderItem={renderExpenseItem}
               keyExtractor={item => item.id}
@@ -161,5 +172,11 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     alignItems: 'center',
     marginVertical: 3,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
